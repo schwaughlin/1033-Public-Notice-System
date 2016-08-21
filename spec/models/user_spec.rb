5cd533a75b19a4620bc96210d492fd8e80ee8aca
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2016-08-21 15:13:20 +0100

    pursposefully break travis

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'Associations' do
     it "should pass a basic test" do
-      expect(true).to eq(true)
+      expect(true).to eq(false)
     end
   end
 
