1f6f520ef2bad1e9147387c5e7fb98d5003255ad
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2016-09-27 15:06:38 +0100

    generates model and controller for equipment

---
+++ db/migrate/20160927140609_create_equipment.rb
@@ -0,0 +1,14 @@
+class CreateEquipment < ActiveRecord::Migration[5.0]
+  def change
+    create_table :equipment do |t|
+      t.string, :department
+      t.string, :code
+      t.string, :name
+      t.string, :cost
+      t.string, :date_acquired
+      t.boolean :records_shown
+
+      t.timestamps
+    end
+  end
+end
