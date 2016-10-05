e9c8176e39044317e73ba67d70ed7b56ea55c595
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2016-10-05 16:38:51 +0100

    shows whether equipment needs to meet public notice requirements

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,3 +1,11 @@
 class Equipment < ApplicationRecord
   has_many :subscriptions
+
+  def require_public_notice?
+    if date_acquired
+      (date_acquired.try(:to_date) > "Sat, 04 Apr 2015".to_date && department != 'DOJ/DEA BILLINGS') ? true : false
+    else
+      false
+    end
+  end
 end
