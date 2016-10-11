9a10e2d5fcdba0157b97bd5153b9d3c0659ddbdc
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2016-10-11 16:47:34 +0100

    #11 starts whitelisting law enforcement emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  WHITELISTED_DEPARTMENTS = %w(@greatfallsmt.net tcsheriff6@3rivers.net tcsheriff1@3rivers.net @sheriff.meagherco.org)
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :subscriptions
 
   scope -> :law_enforcement, ->{ where(law_enforcement: true) }
+
 end
