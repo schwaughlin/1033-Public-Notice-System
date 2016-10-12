23767004abb11c1b97fb60e582e4338b68b84c5a
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2016-10-12 14:16:45 +0100

    #12 tweaks index layout of equipment page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :law_enforcement) }
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :law_enforcement, :department) }
     end
 end
